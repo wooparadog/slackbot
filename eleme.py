6c9ef9bd71abb721ae11359bae73a01d7962ed52
WooParadog <haochuan.guo@ele.me>
2016-09-22 12:19:38 +0800

    First commit

---
+++ eleme.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 WooParadog <guohaochuan@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+
+import re
+import random
+import redis
+
+from slackbot.bot import respond_to, listen_to
+from google import lucky
+
+r = redis.Redis(host="10.0.38.51", port=8889)
+
+HI_MSGS = [
+    "Bite my shiny metal ass",
+    "Kill all humans",
+]
+
+KEYWORD_PREFIX = "slackbot:eleme:keyworkd:%s"
+ALL_KEYWORDS = "slackbot:eleme:keywords"
+
+
+@respond_to('^hi$', re.IGNORECASE)
+def hi(message):
+    message.reply(random.choice(HI_MSGS))
+    message.react('+1')
+
+
+@respond_to('^!(\w+)$')
+@listen_to('^!(\w+)$')
+def keyword_lookup(message, keyword):
+    resp = r.get(KEYWORD_PREFIX % keyword)
+    if not resp:
+        return message.send("Such word, so 404")
+    message.send(resp)
+
+
+@respond_to('^!set (\w+) (.+)$')
+@listen_to('^!set (\w+) (.+)$')
+def set_keyword(message, keyword, value):
+    r.set(KEYWORD_PREFIX % keyword, value)
+    message.send("Got it")
+    r.sadd(ALL_KEYWORDS, keyword)
+
+
+@listen_to("^!keywords$")
+@respond_to("^!keywords$")
+def all_keywords(message, keyword):
+    message.send(','.join(r.smembers(ALL_KEYWORDS)))
+
+
+@respond_to('I love you')
+def love(message):
+    message.reply('I love you too!')
+
+
+@listen_to("^!google (.*)$")
+@respond_to("^!google (.*)$")
+def google(message, keyword):
+    message.send("http://lmgtfy.com/?q={}".format("+".join(keyword.split())))
+
+
+@listen_to("^!g (.*)$")
+@respond_to("^!g (.*)$")
+def google_lucky(message, keyword):
+    r = lucky(keyword)
+    if r:
+        url, desc = r
+        return message.send("{} - {}".format(url, desc))
+    return message.send("Found nothing")
