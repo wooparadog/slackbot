5e3dc80bfc9ba9ad3c007d3c8710137b8086354f
WooParadog <haochuan.guo@ele.me>
2016-11-18 19:50:14 +0800

    f

--- bender/slackbot_settings.py
+++ bender/slackbot_settings.py
@@ -5,3 +5,8 @@ import os
 API_TOKEN = os.environ["API_TOKEN"]
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 ERRORS_TO = 'haochuan.guo'
+
+PLUGINS = [
+        'slackbot.plugins',
+        'mybot',
+        ]
