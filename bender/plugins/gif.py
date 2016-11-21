846024475e875faa1d5137edeaaf892ad64f66a0
kiven <kiven.mr@gmail.com>
2016-11-21 13:01:18 +0800

    Gif search

---
+++ bender/plugins/gif.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.request import quote
+import re
+import requests
+from random import shuffle
+
+
+def unescape(url):
+    # for unclear reasons, google replaces url escapes with \x escapes
+    return url.replace(r"\x", "%")
+
+
+def gif(searchterm, unsafe=False):
+    searchterm = quote(searchterm)
+
+    safe = "&safe=" if unsafe else "&safe=active"
+    searchurl = "https://www.google.com/search?tbs=itp:animated&tbm=isch&q={0}{1}".format(searchterm, safe)
+
+    # this is an old iphone user agent. Seems to make google return good results.
+    useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
+
+    result = requests.get(searchurl, headers={"User-agent": useragent}).text
+
+    gifs = list(map(unescape, re.findall(r"var u='(.*?)'", result)))
+    shuffle(gifs)
+
+    if gifs:
+        return gifs[0]
+    else:
+        return ""
