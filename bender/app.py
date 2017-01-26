567ca630058988db63a8d395d2ae7ba8663940be
WooParadog <haochuan.guo@ele.me>
2017-01-26 10:51:39 +0800

    Configure logging to sentry

--- bender/app.py
+++ bender/app.py
@@ -6,53 +6,10 @@
 #
 # Distributed under terms of the MIT license.
 
-import os
 import flask
-import logging.conf
 from mybot import r, ALL_KEYWORDS
 
 app = flask.Flask(__name__)
-
-
-def config_logging():
-	LOGGING = {
-		'version': 1,
-		'disable_existing_loggers': True,
-
-		'formatters': {
-			'console': {
-				'format': '[%(asctime)s][%(levelname)s] %(name)s '
-						  '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
-				'datefmt': '%H:%M:%S',
-				},
-			},
-
-		'handlers': {
-			'console': {
-				'level': 'DEBUG',
-				'class': 'logging.StreamHandler',
-				'formatter': 'console'
-				},
-			'sentry': {
-				'level': 'ERROR',
-				'class': 'raven.handlers.logging.SentryHandler',
-				'dsn': os.environ.get("SENTRY_DSN", ''),
-				},
-			},
-
-		'loggers': {
-			'': {
-				'handlers': ['console', 'sentry'],
-				'level': 'DEBUG',
-				'propagate': False,
-				},
-			'bender': {
-				'level': 'DEBUG',
-				'propagate': True,
-			},
-		}
-	}
-    logging.config.dictConfig(LOGGING)
 
 
 @app.route('/')
--- bender/main.py
+++ bender/main.py
@@ -1,14 +1,56 @@
 # -*- coding: utf-8 -*-
 
-import logging
-logging.basicConfig()
+import os
+import logging.config
 
-from slackbot.bot import Bot
+
+def config_logging():
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+
+        'formatters': {
+            'console': {
+                'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                          '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+                'datefmt': '%H:%M:%S',
+                },
+            },
+
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'console'
+                },
+            'sentry': {
+                'level': 'ERROR',
+                'class': 'raven.handlers.logging.SentryHandler',
+                'dsn': os.environ.get("SENTRY_DSN", ''),
+                },
+            },
+
+        'loggers': {
+            '': {
+                'handlers': ['console', 'sentry'],
+                'level': 'DEBUG',
+                'propagate': False,
+                },
+            'bender': {
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        }
+    }
+    logging.config.dictConfig(LOGGING)
 
 
 def main():
+    from slackbot.bot import Bot
     bot = Bot()
     bot.run()
 
+
 if __name__ == "__main__":
+    config_logging()
     main()
